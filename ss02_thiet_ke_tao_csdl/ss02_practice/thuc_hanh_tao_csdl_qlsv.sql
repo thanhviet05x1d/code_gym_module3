6519fa46a5b134705aa1d3b5c3aa8b1fbcbff312
viet <thanhvietbkdn@gmail.com>
2023-08-10 13:33:07 +0700

    nop bai

---
+++ ss02_thiet_ke_tao_csdl/ss02_practice/thuc_hanh_tao_csdl_qlsv.sql
@@ -0,0 +1,33 @@
+create database QuanLySinhVien;
+use quanlysinhvien;
+create table class(
+ClassID int not null auto_increment primary key,
+ClassName varchar(60) not null,
+StartDate datetime not null,
+`Status` bit
+);
+create table student(
+StudentID int not null auto_increment primary key,
+StudentName varchar(30) not null,
+Address varchar(50),
+Phone varchar(20),
+`Status` bit,
+ClassID int not null,
+foreign key (ClassID) references Class (ClassID)
+);
+create table subject(
+SubID int not null auto_increment primary key,
+SubName varchar(30) not null,
+Credit tinyint not null default 1 check(Credit>=1),
+`Status` bit default 1
+);
+create table mark(
+MarkID int not null auto_increment primary key,
+SubID int not null,
+StudentID int not null,
+Mark float default 0 check (Mark between 0 and 100 ),
+ExamTimes tinyint default 1,
+unique (SubID,StudentID),
+foreign key (SubID) references subject (SubID),
+foreign key (StudentID) references student (StudentID)
+);
