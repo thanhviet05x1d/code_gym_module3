e3b0096abc0122f74d0b1fe933019d7ecf1246f9
viet <thanhvietbkdn@gmail.com>
2023-08-11 14:04:51 +0700

    nop bai

--- ss02_thiet_ke_tao_csdl/ss02_exercise/chuyen ERD sang mo hinh quan he2.sql
+++ ss02_thiet_ke_tao_csdl/ss02_exercise/chuyen ERD sang mo hinh quan he2.sql
@@ -1,57 +1,67 @@
-DROP TABLE IF EXISTS `vat_tu`;
-CREATE TABLE `vat_tu` (
-  `MaVTU` varchar(20) NOT NULL PRIMARY KEY,
-  `TenVTU` varchar(20) NOT NULL
+create database mo_hinh_quan_he;
+use mo_hinh_quan_he;
+-- drop table if exists `vat_tu`;
+create table `vat_tu` (
+  `ma_vat_tu` varchar(20) primary key,
+  `ten_vat_tu` varchar(20) not null
 );
 
-DROP TABLE IF EXISTS `phieu_xuat`;
-CREATE TABLE `phieu_xuat` (
-  `SoPX` varchar(20) NOT NULL PRIMARY KEY ,
-  `NgayXuat` date NOT NULL
+-- drop table if exists `phieu_xuat`;
+create table `phieu_xuat` (
+  `so_phieu_xuat` varchar(20) primary key ,
+  `ngay_xuat` date not null
 );
 
-DROP TABLE IF EXISTS `chi_tiet_phieu_xuat`;
-CREATE TABLE `chi_tiet_phieu_xuat` (
-  `DGXuat` double NOT NULL,
-  `SLXuat` int NOT NULL
+-- drop table if exists `chi_tiet_phieu_xuat`;
+create table `chi_tiet_phieu_xuat` (
+  `so_phieu_xuat` varchar(20),
+  `ma_vat_tu` varchar(20) ,
+  `don_gia_xuat` double not null,
+  `so_luong_xuat` int not null,
+primary key(so_phieu_xuat, ma_vat_tu),
+foreign key(so_phieu_xuat) references phieu_xuat(so_phieu_xuat),
+foreign key(ma_vat_tu) references vat_tu(ma_vat_tu)
 );
 
-DROP TABLE IF EXISTS `phieu_nhap`;
-CREATE TABLE `phieu_nhap` (
-  `SoPN` varchar(20) NOT NULL PRIMARY KEY,
-  `NgayNhap` date NOT NULL
+-- drop table if exists `phieu_nhap`;
+create table `phieu_nhap` (
+  `so_phieu_nhap` varchar(20) primary key,
+  `ngay_nhap` date not null
 );
 
-DROP TABLE IF EXISTS `chi_tiet_phieu_nhap`;
-CREATE TABLE `chi_tiet_phieu_nhap` (
-  `DGNhatp` double NOT NULL,
-  `SLNhap` int NOT NULL
+-- drop table if exists `chi_tiet_phieu_nhap`;
+create table `chi_tiet_phieu_nhap` (
+  `don_gia_nhap` double not null,
+  `so_luong_nhap` int not null,
+  `so_phieu_nhap` varchar(20),
+  `ma_vat_tu` varchar(20),
+primary key (so_phieu_nhap, ma_vat_tu),
+foreign key(so_phieu_nhap) references phieu_nhap(so_phieu_nhap),
+foreign key(ma_vat_tu) references vat_tu(ma_vat_tu)
 );
 
-DROP TABLE IF EXISTS `don_dat_hang`;
-CREATE TABLE `don_dat_hang` (
-  `SoDH` varchar(20) NOT NULL PRIMARY KEY,
-  `NgayDH` date NOT NULL
+-- drop table if exists `nha_cung_cap`;
+create table `nha_cung_cap` (
+  `ma_nha_cung_cap` varchar(20) primary key,
+  `ten_nha_cung_cap` varchar(20) not null,
+  `dia_chi` varchar(45) not null,
+  `so_dien_thoai` varchar(20) not null
 );
 
-DROP TABLE IF EXISTS `chi_tiet_dat_hang`;
-CREATE TABLE `chi_tiet_dat_hang` (
-  `SoDH` int NOT NULL AUTO_INCREMENT,
-  `MaVTU` varchar(20) NOT NULL,
-  PRIMARY KEY (`SoDH`)
+-- drop table if exists `don_dat_hang`;
+create table `don_dat_hang` (
+    `so_don_hang` varchar(20) primary key,
+    `ngay_dat_hang` date not null,
+	`ma_nha_cung_cap` varchar(20),
+	foreign key(ma_nha_cung_cap) references nha_cung_cap(ma_nha_cung_cap)
 );
 
-DROP TABLE IF EXISTS `nha_cung_cap`;
-CREATE TABLE `nha_cung_cap` (
-  `MaNCC` varchar(20) NOT NULL PRIMARY KEY,
-  `TenNCC` varchar(20) NOT NULL,
-  `DiaChi` varchar(45) NOT NULL,
-  `SDT` int NOT NULL
+-- drop table if exists `chi_tiet_don_dat_hang`;
+create table `chi_tiet_don_dat_hang` (
+  `so_don_hang` varchar(20),
+  `ma_vat_tu` varchar(20),
+  primary key(so_don_hang, ma_vat_tu),
+  foreign key(so_don_hang) references don_dat_hang(so_don_hang),
+  foreign key(ma_vat_tu) references vat_tu(ma_vat_tu)
 );
-
-
-
-
-
-
 
